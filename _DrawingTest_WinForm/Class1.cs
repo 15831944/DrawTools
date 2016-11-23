6ed61b94ca1602a9634bbc9a968cdd0a1752a929
619086871 <619086871@qq.com>
2016-11-23 13:22:18 +0800

    添加两个测试项目”_DrawingTest_WinForm“与”_DrawingTest_WPF“ 1. 这两个项目只是单纯的技术测试项目，用来测试在WinForm与WPF平台下的绘图操作。

---
+++ _DrawingTest_WinForm/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class ClassParent
+    {
+        public ClassParent(int index, string name)
+        {
+        }
+    }
+
+    class ClassChild : ClassParent
+    {
+        public ClassChild() : this(10)
+        {
+      
+        }
+        public ClassChild(int index) : base(index, "")
+        {
+        }
+            
+        }
+    }
+
---
+++ _DrawingTest_WinForm/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        int index;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+   
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+            // Define transformation for container.
+            Rectangle srcRect = new Rectangle(0, 0, 200, 200);
+            Rectangle destRect = new Rectangle(100, 100, 150, 150);
+
+            // Begin graphics container.
+            GraphicsContainer containerState = e.Graphics.BeginContainer(
+                destRect, srcRect,
+                GraphicsUnit.Pixel);
+
+            // Fill red rectangle in container.
+            e.Graphics.FillRectangle(new SolidBrush(Color.Red), 0, 0, 300, 200);
+
+            // End graphics container.
+            e.Graphics.EndContainer(containerState);
+
+            // Fill untransformed rectangle with green.
+            e.Graphics.FillRectangle(new SolidBrush(Color.Green), 0, 0, 200, 200);
+
+        }
+    }
+}
